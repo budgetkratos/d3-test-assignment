14ed36b039ad0dc3dfba0aa196d728893430a8d8
Dusan Rajlic <dusan.rajlic1@gmail.com>
2021-04-24 12:46:35 +0200

    Add comments along with code polish and prettier formatting

--- src/App.js
+++ src/App.js
@@ -1,60 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import './App.css';
-import Chart from './features/Multiples/Chart';
-import * as d3 from 'd3';
-import inputData from './features/Multiples/data/Multiples.csv';
-
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Chart from "./features/Multiples/Chart";
+import * as d3 from "d3";
+import inputData from "./features/Multiples/data/Multiples.csv";
 
 function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     d3.csv(inputData).then((inputData) => {
-        setData([...inputData])
+      setData([...inputData]);
     });
-}, []);
+  }, []);
 
-// Group data into map of maps
-const groupByCategory = d3.group(data, d => d.Category);
-const groupByDistrict = d3.group(data, d => d.District);
+  // Group data into map of maps
+  const groupByCategory = d3.group(data, (d) => d.Category);
+  const groupByDistrict = d3.group(data, (d) => d.District);
 
-// Define carrier of data for sum of individual row values
-let rowValues = [];
+  // Define carrier of data for sum of individual row values
+  let rowValues = [];
 
-// Reduce group entries to a sum of each row, then push name of the row and its
-// sum to ''rowValues'' array
-groupByCategory.forEach((entries, i) => {
-  const rowSum = entries.reduce((a, row) => {
-    const rowValue = parseInt(row['This Year Sales'].substring(1));
-    return a + rowValue;
-  }, 0);
+  // Reduce group entries to a sum of each row, then push name of the row and its
+  // sum to ''rowValues'' array
+  groupByCategory.forEach((entries, i) => {
+    const rowSum = entries.reduce((a, row) => {
+      const rowValue = parseInt(row["This Year Sales"].substring(1));
+      return a + rowValue;
+    }, 0);
 
-  rowValues.push({
-    name: entries[0].Category,
-    value: rowSum,
+    rowValues.push({
+      name: entries[0].Category,
+      value: rowSum,
+    });
   });
-});
-const rowNameOrder = [];
+  const rowNameOrder = [];
 
-// Sort array from min to max value in order to get a blueprint how the standard
-// chart data object will look like, then push row names to new array
-// eslint-disable-next-line array-callback-return
-rowValues.sort((a, b) =>  b.value - a.value).map(entry => {
-  rowNameOrder.push(entry.name);
-});
+  // Sort array from min to max value in order to get a blueprint how the standard
+  // chart data object will look like, then push row names to new array
+  // eslint-disable-next-line array-callback-return
+  rowValues
+    .sort((a, b) => b.value - a.value)
+    // eslint-disable-next-line array-callback-return
+    .map((entry) => {
+      rowNameOrder.push(entry.name);
+    });
 
-// Sort the order of rows in the map object
-groupByDistrict.forEach(entry => {
-  entry.sort(function(a, b) {
-    return rowNameOrder.indexOf(a.Category) - rowNameOrder.indexOf(b.Category);
+  // Sort the order of rows in the map object
+  groupByDistrict.forEach((entry) => {
+    entry.sort(function (a, b) {
+      return (
+        rowNameOrder.indexOf(a.Category) - rowNameOrder.indexOf(b.Category)
+      );
+    });
   });
-})
 
   return (
     <div className="Container">
       {Array.from(groupByDistrict).map((entry) => {
-        return <Chart key={entry[0]} data={entry[1]} title={entry[0]}/>
+        return <Chart key={entry[0]} data={entry[1]} title={entry[0]} />;
       })}
     </div>
   );
--- src/features/Multiples/Chart.jsx
+++ src/features/Multiples/Chart.jsx
@@ -1,70 +1,78 @@
-import React, {  useEffect, useRef } from 'react';
-import * as d3 from 'd3';
-import './Chart.css';
-
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
+import "./Chart.css";
 
 function Chart(props) {
-    const { data, title } = props;
+  const { data, title } = props;
 
-    const svgRef = useRef(null);
+  const svgRef = useRef(null);
 
+  useEffect(() => {
+    const margin = { top: 30, right: 70, bottom: 100, left: 0 },
+      width = 220,
+      height = 150;
 
-
-    useEffect(() => {
-        const margin = {top: 30, right: 70, bottom: 100, left: 0},
-        width = 220,
-        height = 150;
-
-        const svg = d3.select(svgRef.current)
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+    // Canvas selection and drawing positioning
+    const svg = d3
+      .select(svgRef.current)
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
       .append("g")
-        .attr("transform",
-              "translate(" + (width + margin.right) + "," + margin.top + ")rotate(90)");
-    
-        const x = d3.scaleBand()
-                .range([ 0, width ])
-                .domain(data.map(function(d) { return d.Category; }))
-                .padding(0.2);
-    
-        svg.append("g")
-                .attr("transform", "translate(0," + height + ")")
-                .call(d3.axisBottom(x)
-                .tickSize(0))
-                .selectAll("text")
-                .attr("transform", "translate(-10,20)rotate(-90)")
-                .style("text-anchor", "end");
-    
-        
-        const y = d3.scaleLinear()
-                  .domain([0, 1110000])
-                  .range([height, 0])
-        svg.append("g");
-    
-        
-        svg.selectAll("mybar")
-        .data(data)
-        .enter()
-        .append("rect")
-          .attr("x", function(d) { return x(d.Category); })
-          .attr("y", (d) => {
-            const value = parseInt(d['This Year Sales'].substring(1));
-            return height - (height - y(value));
-          })
-          .attr("width", x.bandwidth())
-          .attr("height", function(d) {
-            const value = parseInt(d['This Year Sales'].substring(1));
-              return height - y(value); 
-            })
-          .attr("fill", "black")
-    }, [data])
+      .attr(
+        "transform",
+        "translate(" + (width + margin.right) + "," + margin.top + ")rotate(90)"
+      );
 
-    return (
-        <div className='chart-wrapper'>
-            <h3>{title}</h3>
-            <svg ref={svgRef}/>
-        </div>
-    )
+    // X Axis data selection and drawing
+    const x = d3
+      .scaleBand()
+      .range([0, width])
+      .domain(
+        data.map(function (d) {
+          return d.Category;
+        })
+      )
+      .padding(0.2);
+
+    svg
+      .append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x).tickSize(0))
+      .selectAll("text")
+      .attr("transform", "translate(-10,20)rotate(-90)")
+      .style("text-anchor", "end");
+
+    // TODO: Parametric domain, based on data input
+    // Y Axis data selection and drawing
+    const y = d3.scaleLinear().domain([0, 1110000]).range([height, 0]);
+
+    // Bar drawing
+    svg
+      .selectAll("bar")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", function (d) {
+        return x(d.Category);
+      })
+      .attr("y", (d) => {
+        const value = parseInt(d["This Year Sales"].substring(1));
+        return height - (height - y(value));
+      })
+      .attr("width", x.bandwidth())
+      .attr("height", function (d) {
+        const value = parseInt(d["This Year Sales"].substring(1));
+        return height - y(value);
+      })
+      .attr("fill", "black");
+  }, [data]);
+
+  return (
+    <div className="chart-wrapper">
+      <h3>{title}</h3>
+      <svg ref={svgRef} />
+    </div>
+  );
 }
 
 export default Chart;
